fc13a5c7447ca1f7603d89ff53ef6f3b2adaac6f
NahrinMalakes <nahrinmalakes12@gmail.com>
2023-02-08 12:53:26 +0200

    react-query devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 import { api } from "@/utils/api";
@@ -20,6 +21,10 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      {process.env.NODE_ENV === "development" && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
+
       <ChakraProvider resetCSS={true} theme={theme}>
         <Headers />
         <Component {...pageProps} />
